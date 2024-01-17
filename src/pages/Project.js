2d0d2b3f14fd64f63b8c8f32cc0a0990a97aa2b8
aisyahalghf <alghifariaisyahputri@gmail.com>
2024-01-17 10:19:46 +0700

    fixing project image

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -3,7 +3,7 @@ import ImageCard from "../componen/ImageCard";
 const Project = ({ mode }) => {
   const data = [
     {
-      img: "https://drive.google.com/uc?export=view&id=1OyciYIi8C4VLE8-B2HPngs0cWvGqUWgL",
+      img: "https://xfxbklalncxxnvexsatq.supabase.co/storage/v1/object/public/tumbnail/tumbnail_goku.png?t=2024-01-17T03%3A16%3A53.845Z",
       title: "GoKu App",
       Link: "",
       github: ["https://github.com/ridhozhr10/JCWDOL00807"],
@@ -30,7 +30,7 @@ const Project = ({ mode }) => {
         "GoKu is an Online Grocery Web App that is an e-commerce application that enables online purchases with store location options. These stores have branches in different locations and sell the same products. Users are divided into customers (buyers) and admins (store managers). This application has customizable discount features that can be applied during the purchase of items.",
     },
     {
-      img: "https://drive.google.com/uc?export=view&id=1diCOerxX3O5i54BTuVq62nu3skm0f9v-",
+      img: "https://xfxbklalncxxnvexsatq.supabase.co/storage/v1/object/public/tumbnail/tumbnail_portfolio.png?t=2024-01-17T03%3A17%3A24.162Z",
       title: "Portfolio",
       Link: "https://portofolio-ten-lovat.vercel.app/",
       github: ["https://github.com/aisyahalghf/portofolio"],
@@ -46,7 +46,7 @@ const Project = ({ mode }) => {
       about: `Showcase the projects I have developed and provide comprehensive information about my background and abilities. Through this portfolio, I can present a diverse range of works in the field of Web Development. Furthermore, the portfolio serves as a platform to introduce myself at a more personal level, including my work experience, education, and skills. As a result, the portfolio aims to offer insights into my capabilities and instill confidence in potential collaborators who are interested in working with me.`,
     },
     {
-      img: "https://drive.google.com/uc?export=view&id=1LSX6atcd-IvobKMaxvOQ_fKcusIlCzpb",
+      img: "https://xfxbklalncxxnvexsatq.supabase.co/storage/v1/object/public/tumbnail/tumbnail_whitebus.png?t=2024-01-17T03%3A17%3A38.878Z",
       title: "White Buss App",
       Link: "",
       github: [
@@ -74,7 +74,7 @@ const Project = ({ mode }) => {
         "White Buss App is a web application where users can book a travel bus and choose their seats. Users can also make payments directly by submitting a photo proof of payment.",
     },
     {
-      img: "https://drive.google.com/uc?export=view&id=1cau8TuhW4jpL6sQ_0WV_LVGJEsDvv3TX",
+      img: "https://xfxbklalncxxnvexsatq.supabase.co/storage/v1/object/public/tumbnail/tumbnail_hashtagram.png?t=2024-01-17T03%3A17%3A52.001Z",
       title: "Hashtagram",
       Link: "",
       github: [
@@ -103,7 +103,7 @@ const Project = ({ mode }) => {
         "Hashtagram is a Web App where users can register and log in. They can upload photos, delete and edit them. Users can also interact with other users by writing comments in the comment section.",
     },
     {
-      img: "https://drive.google.com/uc?export=view&id=1sBYgSk-xfiGBCzRDKG7e0tXJXeKONdXV",
+      img: "https://xfxbklalncxxnvexsatq.supabase.co/storage/v1/object/public/tumbnail/Screenshot%202023-06-09%20at%2009.22.18%20(2).png?t=2024-01-17T03%3A18%3A07.964Z",
       title: "SendSage App",
       Link: "https://send-sage.vercel.app",
       tech: [
@@ -122,7 +122,7 @@ const Project = ({ mode }) => {
         "SendSage is specifically designed for effortless checking of intercity shipping costs. By utilizing the free open API provided by Raja Ongkir, users can easily access accurate and up-to-date shipping rates.",
     },
     {
-      img: "https://drive.google.com/uc?export=view&id=1SjNjV49RzT5gi0bqxkBfH_HKj9HrvpYk",
+      img: "https://xfxbklalncxxnvexsatq.supabase.co/storage/v1/object/public/tumbnail/tumbnail_yayasan_aldanazar.png?t=2024-01-17T03%3A18%3A22.106Z",
       title: "Website TK Al-Danazar",
       Link: "https://tk-aldanazar-pamulihan.vercel.app/",
       tech: ["ReactJS", "Javascript", "Node", "Tailwind", "Chakra UI"],
@@ -131,7 +131,7 @@ const Project = ({ mode }) => {
         "This website can be used by users to get the latest information about registration or activities at Al-Danazar Kindergarten. This website is still in the development stage for the parent dashboard. ",
     },
     {
-      img: "https://drive.google.com/uc?export=view&id=17hV3pwMVXYvcIYUpCWDKpIrTkVX2nPpV",
+      img: "https://xfxbklalncxxnvexsatq.supabase.co/storage/v1/object/public/tumbnail/tumbnail_todolist.png?t=2024-01-17T03%3A18%3A36.226Z",
       title: "TodoList",
       Link: "",
       tech: ["ReactJS", "Javascript", "Node", "Tailwind", "Chakra UI"],
@@ -143,7 +143,7 @@ const Project = ({ mode }) => {
         "Build a web application that is useful to help users manage their time and ensure that they have completed their work on time.",
     },
     {
-      img: "https://drive.google.com/uc?export=view&id=1lzOWddeGo9chMrxmWNnIHlOHHe3Lb8PT",
+      img: "https://xfxbklalncxxnvexsatq.supabase.co/storage/v1/object/public/tumbnail/tumbnail_starbuck.png?t=2024-01-17T03%3A18%3A52.307Z",
       title: "Starbucks Clone",
       Link: "",
       tech: ["ReactJS", "Javascript", "Node", "Tailwind", "Chakra UI"],
@@ -157,9 +157,9 @@ const Project = ({ mode }) => {
   ];
 
   return (
-    <section className="container mx-auto flex justify-center mt-10 h-[100vh] px-5 md:px-0 xl:px-5">
-      <div className="flex flex-col gap-10">
-        <div className=" font-extrabold text-5xl  ">Project.</div>
+    <section className='container mx-auto flex justify-center mt-10 h-[100vh] px-5 md:px-0 xl:px-5'>
+      <div className='flex flex-col gap-10'>
+        <div className=' font-extrabold text-5xl  '>Project.</div>
         <ImageCard itemData={data} mode={mode} />
       </div>
     </section>
